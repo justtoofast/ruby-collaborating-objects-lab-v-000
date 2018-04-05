9215359e48ad3f0f7c20a57bb0ede24b0ba4abb6
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-05 00:33:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,15 @@ class Song
   end
 
   def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 end
