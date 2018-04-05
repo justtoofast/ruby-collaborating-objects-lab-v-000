fcf70ff7c4d44b12f6386bc5813d85d3fd59e705
Trac Nguyen <nguyendtrac@gmail.com>
2018-04-05 00:25:34 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
+end
